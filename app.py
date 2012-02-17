ce83537ad23bfd32eb2280e8e79c77cd6621a488
Matthew Williams <mwilliams@HVTL0158.(none)>
2012-02-17 11:55:21 +0000

    need to be on this port

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def relates(post_a_id, post_b_id):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=port)
